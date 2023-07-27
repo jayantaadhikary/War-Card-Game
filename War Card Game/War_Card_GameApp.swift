ec7fc0d341b99721ea3e0fbf41e46e8c495eeeef
Jayanta Adhikary <adhikaryjayanta@outlook.com>
2023-07-27 16:29:29 +0530

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Jayanta Adhikary on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
